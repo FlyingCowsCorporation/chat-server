2893c20625fda5267d851cbcbc7c6074238cf479
Johan Geluk <johan@geluk.io>
2019-02-16 15:49:54 +0100

    Move http code to http file

---
+++ src/http.rs
@@ -0,0 +1,63 @@
+use regex::Captures;
+use regex::RegexBuilder;
+
+pub struct HttpFormatter { }
+
+impl HttpFormatter {
+    pub fn ok(body : &str) -> String {
+        let mut headers = HttpFormatter::format_headers("200 OK");
+        headers.push_str(body);
+        headers
+    }
+
+    pub fn format_headers(status : &str) -> String{
+        let mut headers = String::from("HTTP/1.1 ");
+        headers.push_str(status);
+        headers.push_str("\r\n\r\n");
+        headers
+    }
+}
+
+pub struct HttpParser {}
+
+impl HttpParser {
+    pub fn parse(request_str : &str) -> Result<HttpRequest, &'static str> {
+
+        let re = RegexBuilder::new(r"([^\s]+) ([^\s]+) HTTP/.\..\r\n(.*)\r\n\r\n(.*)")
+            .dot_matches_new_line(true)
+            .build().unwrap();
+        
+        match re.captures(request_str) {
+            Some(captures) => HttpParser::match_to_request(captures),
+            None => {
+                Err("Invalid request.")
+            }
+        }
+    }
+
+    fn match_to_request(captures : Captures) -> Result<HttpRequest, &'static str> {
+        let method = captures.get(1).map_or("", |m| m.as_str());
+        let location = captures.get(2).map_or("", |m| m.as_str()).to_owned();
+        let headers = captures.get(3).map_or("", |m| m.as_str()).to_owned();
+        let body = captures.get(4).map_or("", |m| m.as_str()).to_owned();
+
+        Ok(match method {
+            "GET" => HttpRequest::GET(HttpRequestContent {location, headers, body}),
+            "POST" => HttpRequest::POST(HttpRequestContent {location, headers, body}),
+            _ => {
+                return Err("Invalid method");
+            }
+        })
+    }
+}
+
+pub enum HttpRequest {
+    GET(HttpRequestContent),
+    POST(HttpRequestContent),
+}
+
+pub struct HttpRequestContent{
+    pub location : String,
+    pub headers: String,
+    pub body : String,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
+mod http;
+
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
-use std::result::Result;
-use regex::Captures;
-use regex::RegexBuilder;
+
+use http::HttpFormatter;
+use http::HttpParser;
+use http::HttpRequest;
+
 
 fn main() {
     let listener = TcpListener::bind("0.0.0.0:15000").unwrap();
@@ -41,67 +45,6 @@ fn handle_connection(stream : TcpStream) {
         },
     }
     println!("  > Connection closed.");
-}
-
-enum HttpRequest {
-    GET(HttpRequestContent),
-    POST(HttpRequestContent),
-}
-
-struct HttpRequestContent{
-    location : String,
-    headers: String,
-    body : String,
-}
-
-struct HttpParser {}
-
-impl HttpParser {
-    fn parse(request_str : &str) -> Result<HttpRequest, &'static str> {
-
-        let re = RegexBuilder::new(r"([^\s]+) ([^\s]+) HTTP/.\..\r\n(.*)\r\n\r\n(.*)")
-            .dot_matches_new_line(true)
-            .build().unwrap();
-        
-        match re.captures(request_str) {
-            Some(captures) => HttpParser::match_to_request(captures),
-            None => {
-                Err("Invalid request.")
-            }
-        }
-    }
-
-    fn match_to_request(captures : Captures) -> Result<HttpRequest, &'static str> {
-        let method = captures.get(1).map_or("", |m| m.as_str());
-        let location = captures.get(2).map_or("", |m| m.as_str()).to_owned();
-        let headers = captures.get(3).map_or("", |m| m.as_str()).to_owned();
-        let body = captures.get(4).map_or("", |m| m.as_str()).to_owned();
-
-        Ok(match method {
-            "GET" => HttpRequest::GET(HttpRequestContent {location, headers, body}),
-            "POST" => HttpRequest::POST(HttpRequestContent {location, headers, body}),
-            _ => {
-                return Err("Invalid method");
-            }
-        })
-    }
-}
-
-struct HttpFormatter { }
-
-impl HttpFormatter {
-    fn ok(body : &str) -> String {
-        let mut headers = HttpFormatter::format_headers("200 OK");
-        headers.push_str(body);
-        headers
-    }
-
-    fn format_headers(status : &str) -> String{
-        let mut headers = String::from("HTTP/1.1 ");
-        headers.push_str(status);
-        headers.push_str("\r\n\r\n");
-        headers
-    }
 }
 
 struct Connection{
